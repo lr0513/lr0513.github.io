b9f51b32765f3ffdce3ba3e3dd2090c00876b3e3
lr <15941174592@163.com>
2025-03-21 11:39:35 +0800

    Site updated: 2025-03-21 11:39:35

--- js/title.js
+++ js/title.js
@@ -4,11 +4,11 @@ var titleTime;
 document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
         //离开当前页面时标签显示内容
-        document.title = '👀跑哪里去了~';
+        document.title = '燕子，你不要走啊😭~';
         clearTimeout(titleTime);
     } else {
         //返回当前页面时标签显示内容
-        document.title = '🐖抓到你啦～';
+        document.title = '燕子～';
         //两秒后变回正常标题
         titleTime = setTimeout(function () {
             document.title = OriginTitile;
